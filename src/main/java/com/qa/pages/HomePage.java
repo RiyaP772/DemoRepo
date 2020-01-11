74c9ad704bdb16e72f15e84bfaf036b7e315d62a
jasmeetnarula <jasmeet.narula43@gmail.com>
2020-01-11 18:56:40 -0500

    first git commit for my selenium code

---
+++ src/main/java/com/qa/pages/HomePage.java
@@ -0,0 +1,10 @@
+package com.qa.pages;
+
+public class HomePage {
+	
+	public void test(){
+		System.out.println("home page");
+		
+	}
+
+}
---
+++ src/main/java/com/qa/util/testUtil.java
@@ -0,0 +1,10 @@
+package com.qa.util;
+
+public class testUtil {
+	
+	public void test(){
+		System.out.println("Util Page");
+		
+	}
+
+}
